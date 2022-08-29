ec639f21f0349f30f0c30e6df47d99f76b254d13
Marcelo Eduardo Rufino <fmarcelo3duardo@gmail.com>
2022-08-29 15:50:19 -0300

    timeout ok

--- cliente_A.py
+++ cliente_A.py
@@ -1,17 +1,21 @@
-import asyncio
 import socket
 import time
 
+#globais
+HOST = '192.168.26.28'
+PORT = 5005
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp.settimeout(2)
 
-async def cliente_A():
-    HOST = '192.168.26.28'
-    PORT = 5005
+def cliente_A():
+    #HOST = '192.168.26.28'
+    #PORT = 5005
 
-    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     alter = 0
     #udp.connect((HOST, PORT))
     while True:
-        mensagemVoltou = '!'
+        mensagemVoltou = bytes('!',"utf-8")
         if alter == 0: 
             mensagem = '0|'
             alter = 1
@@ -21,28 +25,33 @@ async def cliente_A():
             
         mensagem += input('digite a mensagem : ')
         print(mensagem)
-        time_init = time.time()
-        udp.sendto(bytes(mensagem,"utf-8"),(HOST, PORT))
+        
+       
             
         mensagem = str(mensagem)
         
         #rodar em paralelo async
-        
-        while mensagemVoltou == '!':
-            mensagemVoltou, endereço_cliente = await udp.recvfrom(1024)
-            time_fin = time.time()
-            tempo_corrido = time_fin - time_fin
-            if mensagemVoltou[0] == '!' > 1 and tempo_corrido > 200 :
-                #reenvia a mensagem
-                udp.sendto(bytes(mensagem,"utf-8"),(HOST, PORT))
+        ack_r = False
+        while not ack_r:
+            #envio
+            udp.sendto(bytes(mensagem,"utf-8"),(HOST, PORT))
+            time_init = time.time()
+            try:
+                mensagemVoltou, endereço_cliente = udp.recvfrom(1024)
+            except socket.timeout:
+                print("Timeout")
+            else:
+                print('mensagem ->', mensagemVoltou.decode('utf-8'))
+                ack_r = True
 
         #mensagemconfirm, endereço_cliente = udp.recvfrom(1024)
-        
-    
-        print('Mensagem Recebida:---> ',mensagemVoltou.decode('utf-8'))
+
+        #print('Mensagem Recebida:---> ',mensagemVoltou.decode('utf-8'))
         #print('confirmação de mensagem:',mensagemconfirm.decode('utf-8'))
         if mensagem[2] == '&' :
                 break
     udp.close()
 
-asyncio.run(cliente_A())
+
+
+cliente_A()
