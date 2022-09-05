1ace52895d9909dd26fc1c2391f705dcc8408506
Marcelo Eduardo Rufino <fmarcelo3duardo@gmail.com>
2022-09-05 17:47:14 -0300

    tentativa checksum

--- cliente_A.py
+++ cliente_A.py
@@ -4,7 +4,7 @@ import select
 import sys
 
 #globais
-HOST = '192.168.15.11' #'192.168.26.28'
+HOST = '192.168.43.28' #'192.168.26.28'
 PORT = 5002
 PORT_MYA = 5001
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -19,17 +19,25 @@ def cliente_A():
     alter = 0
     #udp.connect((HOST, PORT))
     while True:
-        mensagemVoltou = bytes('!',"utf-8")
+        #mensagemVoltou = bytes('!',"utf-8")
         if alter == 0: 
-            mensagem = '0|'
+            Ack = '0|'
             alter = 1
         else:
-            mensagem = '1|'
+            Ack = '1|'
             alter = 0
         
-        mensagem += input('digite a mensagem : ')
+        k = 1024/4
+       
+        
+        mensagemInput = input('digite a mensagem : ')
+        
+        checkS = str(findChecksum(mensagem,k))
+        mensagem += (Ack,checkS,'|', mensagemInput)
+        
         print(mensagem)
         
+           
         mensagem = str(mensagem)
         
         #rodar em paralelo async
@@ -61,4 +69,36 @@ def cliente_A():
 
 
 
+
+
+def findChecksum(mensagem, k):
+    
+    # Dividindo a mensagem em 4 pacotes de K bits
+    c1 = mensagem[0:k]
+    c2 = mensagem[k:2*k]
+    c3 = mensagem[2*k:3*k]
+    c4 = mensagem[3*k:4*k]
+
+    # Calculando a soma binaria dos pacotes
+    Sum = bin(int(c1, 2)+int(c2, 2)+int(c3, 2)+int(c4, 2))[2:]
+
+    # Adding the overflow bits
+    if(len(Sum) > k):
+        x = len(Sum)-k
+        Sum = bin(int(Sum[0:x], 2)+int(Sum[x:], 2))[2:]
+    if(len(Sum) < k):
+        Sum = '0'*(k-len(Sum))+Sum
+
+    # Calculating the complement of sum
+    Checksum = ''
+    for i in Sum:
+        if(i == '1'):
+            Checksum += '0'
+        else:
+            Checksum += '1'
+        return Checksum
+    
+    return Checksum
+    #mensagem = [str(mensagem), Checksum]
+    
 cliente_A()
